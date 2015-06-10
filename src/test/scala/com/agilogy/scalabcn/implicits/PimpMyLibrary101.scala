2cd728bd1df1a14db5f96c479733412686888ed8
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 16:03:14 +0200

    Step 11: Bonus! Pimp my library without runtime impact

--- src/test/scala/com/agilogy/scalabcn/implicits/PimpMyLibrary101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/PimpMyLibrary101.scala
@@ -2,21 +2,24 @@ package com.agilogy.scalabcn.implicits
 
 import org.scalatest.FlatSpec
 
+object StringUtils{
+  // pimp my library!
+  // syntax sugar!
+  // no runtime impact!
+  implicit class StringHelper(val self:String) extends AnyVal{
+    def nonActuallyEmpty:Boolean = self.trim.length > 0
+  }
+}
+
 class PimpMyLibrary101 extends FlatSpec{
 
   behavior of "pimp my library"
 
-  // pimp my library!
-  // syntax sugar!
-  implicit class StringHelper(self:String){
-    def nonActuallyEmpty:Boolean = self.trim.length > 0
-  }
-
   it should "extend an existing class with new members" in {
+    import StringUtils._
     assert("".nonActuallyEmpty === false)
     assert("  ".nonActuallyEmpty === false)
     assert("  2".nonActuallyEmpty === true)
-
   }
 
 }
