ce52c69e51a75e98014c1aa5d5f66be637887eb6
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 15:54:01 +0200

    Step 10: Pimp my library syntax sugar

--- src/test/scala/com/agilogy/scalabcn/implicits/PimpMyLibrary101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/PimpMyLibrary101.scala
@@ -7,11 +7,10 @@ class PimpMyLibrary101 extends FlatSpec{
   behavior of "pimp my library"
 
   // pimp my library!
-  class StringHelper(self:String){
+  // syntax sugar!
+  implicit class StringHelper(self:String){
     def nonActuallyEmpty:Boolean = self.trim.length > 0
   }
-  implicit def stringToStringHelper(s:String):StringHelper = new StringHelper(s)
-
 
   it should "extend an existing class with new members" in {
     assert("".nonActuallyEmpty === false)
