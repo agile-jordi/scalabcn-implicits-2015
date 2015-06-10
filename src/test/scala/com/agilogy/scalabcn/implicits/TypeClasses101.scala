a5525b4bcbcf89c611e8f9175553f0622fccb123
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 18:03:18 +0200

    Step 14: Typeclasses motivation redux

--- src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
@@ -2,23 +2,30 @@ package com.agilogy.scalabcn.implicits
 
 import org.scalatest.FlatSpec
 
+trait Comparator[T]{
+  def gt(a:T,b:T):Boolean
+}
+
 class TypeClasses101 extends FlatSpec {
 
   behavior of "existing types"
 
   // Polymorphism: max operates on comparable things:
-  def max[T <: Comparable[T]](l:List[T], ifEmpty:T):T = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
-    case (acc,elem) => if(acc.compareTo(elem) > 0) acc else elem
+  def max[T](l:List[T], ifEmpty:T)(comparator:Comparator[T]):T = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
+    case (acc,elem) => if(comparator.gt(acc,elem)) acc else elem
   }
 
   they should "not suck, but they have what they have" in {
     val integers = List(2,42,23,5)
-    // Ouch! Int is not Comparable!
-    val javaInts = integers.map(i => new Integer(i))
-    assert(max(javaInts,new Integer(0)) === new Integer(42))
+    val intComparator = new Comparator[Int] {
+      override def gt(a: Int, b: Int): Boolean = a > b
+    }
+    assert(max(integers,0)(intComparator) === new Integer(42))
     val doubles = List(2.0,42.0,23.0,5.0)
-    // Ouch! Won't compile
-    // assert(max(doubles, 0.0) === 42.0)
+    val doubleComparator = new Comparator[Double]{
+      override def gt(a: Double, b: Double): Boolean = a > b
+    }
+    assert(max(doubles, 0.0)(doubleComparator) === 42.0)
   }
 
 }
