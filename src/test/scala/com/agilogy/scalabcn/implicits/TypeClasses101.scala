dcec636f7a61d0d2b2f795b499b9abb459d5018a
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 17:59:40 +0200

    Step 13: Typeclasses motivation continued

--- src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
@@ -6,13 +6,16 @@ class TypeClasses101 extends FlatSpec {
 
   behavior of "existing types"
 
-  def max(l:List[Int], ifEmpty:Int):Int = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
-    case (acc,elem) => if(acc >= elem) acc else elem
+  // Polymorphism: max operates on comparable things:
+  def max[T <: Comparable[T]](l:List[T], ifEmpty:T):T = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
+    case (acc,elem) => if(acc.compareTo(elem) > 0) acc else elem
   }
 
   they should "not suck, but they have what they have" in {
     val integers = List(2,42,23,5)
-    assert(max(integers,0) === 42)
+    // Ouch! Int is not Comparable!
+    val javaInts = integers.map(i => new Integer(i))
+    assert(max(javaInts,new Integer(0)) === new Integer(42))
     val doubles = List(2.0,42.0,23.0,5.0)
     // Ouch! Won't compile
     // assert(max(doubles, 0.0) === 42.0)
