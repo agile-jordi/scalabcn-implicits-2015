b383d53167a9fa19ccdc5263e5ca5b17a186a777
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 18:07:37 +0200

    Step 15: Basic type class

--- src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
@@ -2,30 +2,38 @@ package com.agilogy.scalabcn.implicits
 
 import org.scalatest.FlatSpec
 
+// Typeclass Comparator
 trait Comparator[T]{
   def gt(a:T,b:T):Boolean
 }
 
+object Comparator{
+  
+  // int is of type Comparator
+  implicit val intComparator = new Comparator[Int] {
+    override def gt(a: Int, b: Int): Boolean = a > b
+  }
+
+  // double is of type Comparator
+  implicit val doubleComparator = new Comparator[Double]{
+    override def gt(a: Double, b: Double): Boolean = a > b
+  }
+}
+
 class TypeClasses101 extends FlatSpec {
 
-  behavior of "existing types"
+  behavior of "type classes"
 
-  // Polymorphism: max operates on comparable things:
-  def max[T](l:List[T], ifEmpty:T)(comparator:Comparator[T]):T = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
+  // Polymorphism: max operates on things of the comparator type
+  def max[T](l:List[T], ifEmpty:T)(implicit comparator:Comparator[T]):T = if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
     case (acc,elem) => if(comparator.gt(acc,elem)) acc else elem
   }
 
-  they should "not suck, but they have what they have" in {
+  they should "provide polymorphism" in {
     val integers = List(2,42,23,5)
-    val intComparator = new Comparator[Int] {
-      override def gt(a: Int, b: Int): Boolean = a > b
-    }
-    assert(max(integers,0)(intComparator) === new Integer(42))
+    assert(max(integers,0) === new Integer(42))
     val doubles = List(2.0,42.0,23.0,5.0)
-    val doubleComparator = new Comparator[Double]{
-      override def gt(a: Double, b: Double): Boolean = a > b
-    }
-    assert(max(doubles, 0.0)(doubleComparator) === 42.0)
+    assert(max(doubles, 0.0) === 42.0)
   }
 
 }
