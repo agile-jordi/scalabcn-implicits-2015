3e26fc47478eb7ed4b14f3acf7a3c51d6ff5b281
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-10 12:12:14 +0200

    Step 5: Basic implicit conversions

--- src/test/scala/com/agilogy/scalabcn/implicits/ImplicitConversions101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/ImplicitConversions101.scala
@@ -4,20 +4,22 @@ import org.scalatest.FlatSpec
 
 class ImplicitConversions101 extends FlatSpec{
 
-  behavior of "code without implicit conversions"
+  behavior of "implicit conversions"
 
   implicit val user = new User("agile_jordi")
   val s = new IssueTrackingService
   val issueId = s.createIssue("let's code without implicit conversions")
 
-  they should "be comfortable" in {
+  implicit def intToIssueId(i:Int):IssueId = new IssueId(i)
+
+  they should "be powerful" in {
 
     // Imagine you got an issue id as an integer from the user
     val intIssueId:Int = issueId.id
 
-    s.addComment(new IssueId(intIssueId),"ouch! I need to create an IssueId instance from the int...")
+    s.addComment(intIssueId,"ouch! I need to create an IssueId instance from the int...")
 
-    assert(s.getIssue(new IssueId(intIssueId)).get.comments.head.comment.startsWith("ouch!"))
+    assert(s.getIssue(intIssueId).get.comments.head.comment.startsWith("ouch!"))
   }
 
 }
