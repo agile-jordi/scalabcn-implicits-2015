82b6b393f8e5780ca7dcc2b8bd1c139549d23449
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-11 10:52:10 +0200

    Step 19: =:=

--- src/test/scala/com/agilogy/scalabcn/implicits/ImplicitTypeBounds.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/ImplicitTypeBounds.scala
@@ -4,21 +4,17 @@ import org.scalatest.FlatSpec
 
 class ImplicitTypeBounds extends FlatSpec{
 
-  // Let's say we want to implement a function to check 2 things for equality...
-  // Just like equals, but we want to fail at compile time if the compiler knows they can't be possibly equals
-
-  def eq[T](a:T,b:T):Boolean = a == b
+  // We want an evidence (ev) that T1 and T2 are indeed the same type
+  // not only that they have a supertype in common
+  def eq[T1,T2](a:T1,b:T2)(implicit ev: T1 =:= T2):Boolean = a == b
 
   behavior of "type safe equality"
 
   it should "correctly compare values of the same type" in {
     assert(eq("hi","hi") === true)
     assert(eq("hey","ho") === false)
-  }
-
-  it should "not compile when given two things that can't be equal" in {
-    assert(eq("hi", 3) === false)
-    // Ouch! How the hell did that compile?
+    // This no longer compiles
+    // assert(eq("hi", 3) === false)
   }
 
 }
