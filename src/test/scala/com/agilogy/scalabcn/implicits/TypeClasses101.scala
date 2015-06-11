85bd593cb7def1e493cf8b9635ed91fe595be921
Jordi Pradel <jordi.pradel@agilogy.com>
2015-06-11 10:11:29 +0200

    Step 17: Ad-hoc polymorphism

--- src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
+++ src/test/scala/com/agilogy/scalabcn/implicits/TypeClasses101.scala
@@ -2,32 +2,13 @@ package com.agilogy.scalabcn.implicits
 
 import org.scalatest.FlatSpec
 
-// Typeclass Comparator
-trait Comparator[T]{
-  def gt(a:T,b:T):Boolean
-}
-
-object Comparator{
-
-  // int is of type Comparator
-  implicit val intComparator = new Comparator[Int] {
-    override def gt(a: Int, b: Int): Boolean = a > b
-  }
-
-  // double is of type Comparator
-  implicit val doubleComparator = new Comparator[Double]{
-    override def gt(a: Double, b: Double): Boolean = a > b
-  }
-}
-
 class TypeClasses101 extends FlatSpec {
 
   behavior of "type classes"
 
-  // Polymorphism: max operates on things of the comparator type
-  // Syntax sugar! Context bounds
-  def max[T:Comparator](l:List[T], ifEmpty:T):T = {
-    val comparator = implicitly[Comparator[T]]
+  // In fact, there is a type class in the standard library
+  def max[T:Ordering](l:List[T], ifEmpty:T):T = {
+    val comparator = implicitly[Ordering[T]]
     if(l.isEmpty) ifEmpty else l.tail.fold(l.head){
       case (acc,elem) => if(comparator.gt(acc,elem)) acc else elem
     }
@@ -38,6 +19,22 @@ class TypeClasses101 extends FlatSpec {
     assert(max(integers,0) === new Integer(42))
     val doubles = List(2.0,42.0,23.0,5.0)
     assert(max(doubles, 0.0) === 42.0)
+  }
+
+  they should "provide ad-hoc polymorphism" in {
+    val agileJordi = User("agile_jordi")
+    val ignasi35 = User("ignasi35")
+    val issue0 = new Issue(IssueId(0), "zeroth", agileJordi, Seq.empty)
+    val issue1 = new Issue(IssueId(1), "foo", agileJordi, Seq.empty)
+    val issue2 = new Issue(IssueId(2), "foo", agileJordi, Seq(IssueComment("first", agileJordi), IssueComment("second", ignasi35)))
+    val issue3 = new Issue(IssueId(3), "foo", agileJordi, Seq(IssueComment("one", ignasi35)))
+    val issues = List(issue1, issue2, issue3)
+
+    implicit val issueOrdering: Ordering[Issue] = new Ordering[Issue] {
+      override def compare(x: Issue, y: Issue): Int = x.comments.size - y.comments.size
+    }
+    assert(max(issues, issue0) === issue2)
+
   }
 
 }
